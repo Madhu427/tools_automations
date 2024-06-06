8ddd2f90231333c8fc5d8d67634d00a6338c5c8b
Madhu427 <madhusudhanachary.k@gmail.com>
2024-06-06 12:30:44 +0530

    prom installation

---
+++ prom_install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+   echo("You should be the root user")
+   exit 1
+fi  
+
+if [ -d /opt/prometheus ]; then
+   rm -rf /opt/prometheus
+   exit 1
+fi
+
+URL = $(curl -L -s https://prometheus.io/download/ | grep tar | grep prometheus | grep linux-amd64.tar.gz | sed -e "s|>| |g" -e "s|<| |g" -e 's|"| |g' |xargs 
+-n1 | grep ^http | tail -1)
+
+
+FILENAME=$(echo $URL | awk -F / '{print $NF}')
+DIRNAME= $(echo $FILENAME | sed -e "s/.tar.gz//") 
+
+
+cd /opt/
+
+curl -s -L -O $URL
+tar -xf $FILENAME
+rm -rf $FILENAME
