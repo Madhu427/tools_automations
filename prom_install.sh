4b2060b6e05c953fa58e5c8dffef92f785340695
Madhu427 <madhusudhanachary.k@gmail.com>
2024-06-06 18:56:12 +0530

    URL corrected

--- prom_install.sh
+++ prom_install.sh
@@ -9,7 +9,7 @@ if [ -d /opt/prometheus ]; then
   rm -rf /opt/prometheus
 fi
 
-URL=$(curl -L -s https://prometheus.io/download/ | grep tar | grep prometheus | grep linux-amd64.tar.gz | sed -e "s|>| |g" -e "s|<| |g" -e 's|"| |g' |xargs -n1 | grep ^http | tail -1)
+URL=$(curl -L -s https://prometheus.io/download/ | grep tar | grep prometheus- | grep linux-amd64.tar.gz | sed -e "s|>| |g" -e "s|<| |g" -e 's|"| |g' |xargs -n1 | grep ^http | tail -1)
 
 
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
