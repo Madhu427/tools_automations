6b850692086adbb2b2b8abb1ae51266ab20c368c
Madhu427 <madhusudhanachary.k@gmail.com>
2024-06-06 20:18:59 +0530

    sudo removed

--- prom_install.sh
+++ prom_install.sh
@@ -26,5 +26,5 @@ echo "$DIRNAME"
 mv $DIRNAME prometheus 
 
 curl -L -s https://github.com/Madhu427/tools_automations/blob/master/prometheus.service > /etc/systemd/system/prometheus.service
-sudo systemctl daemon-reload
-sudo systemctl start prometheus
+systemctl daemon-reload
+systemctl start prometheus
