1d242d9007a04ce9b79efb150c9ac0984ce72245
Madhu427 <madhusudhanachary.k@gmail.com>
2024-06-06 12:37:17 +0530

    IF condition corrected

--- prom_install.sh
+++ prom_install.sh
@@ -1,17 +1,15 @@
 #! /bin/bash
 
 if [ $(id -u) -ne 0 ]; then
-   echo("You should be the root user")
-   exit 1
+  echo "You should be the root user"
+  exit 1
 fi  
 
 if [ -d /opt/prometheus ]; then
-   rm -rf /opt/prometheus
-   exit 1
+  rm -rf /opt/prometheus
 fi
 
-URL = $(curl -L -s https://prometheus.io/download/ | grep tar | grep prometheus | grep linux-amd64.tar.gz | sed -e "s|>| |g" -e "s|<| |g" -e 's|"| |g' |xargs 
--n1 | grep ^http | tail -1)
+URL=$(curl -L -s https://prometheus.io/download/ | grep tar | grep prometheus | grep linux-amd64.tar.gz | sed -e "s|>| |g" -e "s|<| |g" -e 's|"| |g' |xargs -n1 | grep ^http | tail -1)
 
 
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
