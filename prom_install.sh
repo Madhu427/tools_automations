280574e369451c884216d22d7c352ff2b362c82e
Madhu427 <madhusudhanachary.k@gmail.com>
2024-06-07 14:07:21 +0530

    curl command corrected

--- prom_install.sh
+++ prom_install.sh
@@ -25,6 +25,6 @@ echo "$FILENAME"
 echo "$DIRNAME"
 mv $DIRNAME prometheus 
 
-curl -L -s https://github.com/Madhu427/tools_automations/blob/master/prometheus.service >/etc/systemd/system/prometheus.service
+curl -s https://github.com/Madhu427/tools_automations/blob/master/prometheus.service >/etc/systemd/system/prometheus.service
 sudo systemctl daemon-reload
 sudo systemctl start prometheus
